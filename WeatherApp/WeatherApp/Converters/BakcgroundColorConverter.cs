45f7b3e557db7c36ce4660cee8b01ebf095e4d94
nurayklc <nuraykilic0101@gmail.com>
2022-07-25 13:40:04 +0300

    Update main page weather app

---
+++ WeatherApp/WeatherApp/Converters/BakcgroundColorConverter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp.Converters
+{
+    public class BakcgroundColorConverter : ValueConverter
+    {
+    }
+}
